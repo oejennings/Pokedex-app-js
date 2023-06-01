1b946dbd1c14ba05c49e1971794d5344fcdb214d
Olivia Jennings <oejennings@users.noreply.github.com>
2023-06-01 15:10:29 -0400

    Updated pokemonList
    Started pokemoneList variable and added keys to each object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'pasta';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']}, 
+    {name: 'Pikachu', height: 0.4, type: 'electric'},
+    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+    {name: 'Venomoth', height: 1.5, type: ['bug', 'poison']}
+]
