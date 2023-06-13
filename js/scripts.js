a6dcc8b17e30f19d36ec158b9756017c051b1bc2
Olivia Jennings <oejennings@users.noreply.github.com>
2023-06-13 13:20:24 -0400

    Created forEach() Loop and IIFE
    wrapped pokemonList in IIFE variable called pokemonRepository
    changed for() loop to forEach()
    commented if/else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,60 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']}, 
-    {name: 'Pikachu', height: 0.4, type: 'electric'},
-    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-    {name: 'Venomoth', height: 1.5, type: ['bug', 'poison']}
-]
-for (let i=0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.5) {
-        document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' Wow, that\'s big!' + ' ');
+let pokemonRepository = (function () {
+    
+    let pokemonList = [
+        {
+            name: 'Bulbasaur', 
+            height: 0.7, 
+            type: ['grass', 'poison']
+        }, 
+        {
+            name: 'Pikachu', 
+            height: 0.4, 
+            type: 'electric'
+        },
+        {
+            name: 'Charizard', 
+            height: 1.7, 
+            type: ['fire', 'flying']
+        },
+        {
+            name: 'Venomoth', 
+            height: 1.5, 
+            type: ['bug', 'poison']
+        }
+    ];
+
+    function getAll() {
+        return pokemonList;
     }
-    else {
-        document.write('<p>' + pokemonList[i].name + ' ' + pokemonList[i].height + '<p/> ')
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
+
+    return{
+        getAll: getAll,
+        add: add  
+    };
+
+}) ();
+
+
+function listingPokemon (pokemon) {
+
+    document.write('<p>' + pokemon.name + ' ' + pokemon.height + ' ' + pokemon.type + '<p/>'); 
+    
+    // if (pokemonList.height > 1.5) {
+    //     document.write('<p>' + pokemon.name + ' ' + pokemon.height + ' Wow, that\'s big!' + ' ' + pokemon.type + '<p/');
+    // }
+    // else {
+    //     document.write('<p>' + pokemon.name + ' ' + pokemon.height + '<p/> ')
+    // }
+
 }
 
+pokemonRepository.getAll().forEach(listingPokemon);
+
+
+
+
+    
