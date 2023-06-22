bd86c223a121c5072cfce3a5f2d0f69f6954a030
Olivia Jennings <oejennings@users.noreply.github.com>
2023-06-22 14:37:17 -0400

    Created addListItem in IIFE
    Also created a button for each pokemon.
    Created an event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function () {
     
-    let pokemonList = [
+    let pokedex = [
         {
             name: 'Bulbasaur', 
             height: 0.7, 
@@ -24,34 +24,45 @@ let pokemonRepository = (function () {
     ];
 
     function getAll() {
-        return pokemonList;
+        return pokedex;
     }
 
     function add(pokemon) {
-        pokemonList.push(pokemon);
+        pokedex.push(pokemon);
     }
 
+    function addListItem(pokemon) {
+        let pokemonLists = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+
+
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        button.addEventListener('click', function() {
+            showDetails(pokemon);
+        })
+
+        listItem.appendChild(button);
+        pokemonLists.appendChild(listItem); 
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon.name);
+    }
+    
     return{
         getAll: getAll,
-        add: add  
+        add: add,  
+        addListItem: addListItem
     };
-
+    
 }) ();
 
 
-function listingPokemon (pokemon) {
-    
-    if (pokemon.height > 1.5) {
-        document.write('<p>' + pokemon.name + ' ' + pokemon.height + ' (Wow, that\'s big!)' + ' ' + pokemon.type + '</p>');
-    }
-    else {
-        document.write('<p>' + pokemon.name + ' ' + pokemon.height + ' ' + pokemon.type + '</p> ')
-    }
-
-}
-
-
-pokemonRepository.getAll().forEach(listingPokemon);
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
 
 
 
